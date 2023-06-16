c592cd121a9cc0104354d47eb8e6a0220dd8e24a
Jason Louv <40778908+magickme@users.noreply.github.com>
2023-06-15 22:31:14 -0500

    Initial & done

---
+++ script.py
@@ -0,0 +1,11 @@
+# Create calculate_insurance_cost() function below: 
+def calculate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
+    estimated_cost = 250*age - 128*sex + 270*bmi + 425*num_of_children + 24000*smoker - 12500
+    print("The estimated insurance cost for " + str(name) + " is " + str(estimated_cost) + " dollars.")
+    return estimated_cost
+
+maria_insurance_cost = calculate_insurance_cost("Maria", 28, 0, 26.2, 3, 0)
+
+omar_insurance_cost = calculate_insurance_cost("Omar", 35, 1, 22.2, 0, 1)
+
+jason_insurance_cost = calculate_insurance_cost("Jason", 41, 1, 27, 0, 0)
